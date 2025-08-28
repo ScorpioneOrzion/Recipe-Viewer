d80e133799cc5f51a98c005f5319a67254bb7168
ScorpioneOrzion <36512390+ScorpioneOrzion@users.noreply.github.com>
2025-08-28 15:53:33 +0200

    added some styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,28 @@
+import { For } from 'solid-js'
 import './App.css'
 
-function App() {
-	console.log("test1234")
+const pageTypes = ["Tags", "Items", "Machines", "Recipies"]
 
+function App() {
 	return (
 		<>
-			<div>
-				Hello
-			</div>
+			<For each={pageTypes}>
+				{page =>
+					<div class='page-part'>
+						<button type='button' onclick={e => {
+							const nextElement = (e.target.nextElementSibling as HTMLElement)
+
+							nextElement.style.display = {
+								'none': 'flex',
+								'flex': 'none'
+							}[nextElement.style.display as 'none' | 'flex']
+						}}>{page}</button>
+						<ul id={page.toLowerCase()} style={{ display: "none" }}>
+							<li><button type='button'>Add</button></li>
+						</ul>
+					</div>
+				}
+			</For>
 		</>
 	)
 }
